4a751b52ca4a4c7a2a72a5c0e07ab09a8836e657
FEUYAN TCHOUO AUDRIC CAUDIN <audric.feuyan@mboadigital.tech>
2023-06-07 17:46:19 +0100

    Initial and final commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book3.jpg";
+
+export const books = [
+  {
+    title: "Pagny par Florent",
+    author: "Florent Pagny",
+    img: img2,
+    id: 1,
+  },
+  {
+    title: "Sur la dalle",
+    author: "Fred Vargas",
+    img: img1,
+    id: 2,
+  },
+  {
+    title: "Atlas - L'histoire de Pa Salt",
+    author: "Lucinda Riley",
+    img: img3,
+    id: 3,
+  },
+];
